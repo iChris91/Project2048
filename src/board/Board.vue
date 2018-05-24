1ad7ca9a8588a66714576fec0bef15aa4bce3a8c
Chris <michelle.christophe@gmail.com>
2018-05-24 10:52:31 +0200

    first commit

---
+++ src/board/Board.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="board"></div>
+</template>
+<script>
+import board from '@/board/board.js'
+
+export default {
+  name: 'Board',
+  created () {
+    board.init(4)
+  }
+}
+</script>
+<style></style>
